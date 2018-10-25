6320ad98e18bfd95f50cb8bfb1188402fb5efa91
Scssml <scssml@yandex.ru>
2018-10-25 19:15:52 +0400

    Created block goods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -19,103 +19,196 @@
       <v-flex
         xs8
       >
-        <v-layout row style="overflow: auto;">
-          <v-flex
-            xs3
-            v-for="n in 10"
-            :key="n"
+        <v-container
+          id="scroll-block-top"
+          style="overflow: auto;"
+          class="scroll-y pa-0 right-block"
+        >
+          <v-layout
+            row
+            v-scroll:#scroll-block-top="onScroll"
           >
+            <v-flex
+              v-for="n in 10"
+              :key="n"
+            >
+              <v-card
+                flat
+                width="300"
+                style="border-left: 1px solid #ccc; font-size: 16px;"
+              >
+                <div class="px-3" style="height: 48px;">
+                  <v-select
+                    label="Standard"
+                    solo
+                    flat
+                    hide-details
+                  ></v-select>
+                </div>
+                <v-divider></v-divider>
+                <div class="px-3" style="height: 48px;">
+                  <v-select
+                    label="Standard"
+                    solo
+                    flat
+                    hide-details
+                  ></v-select>
+                </div>
+                <v-divider></v-divider>
+                <div class="px-3" style="height: 48px;">
+                  <v-select
+                    label="Standard"
+                    solo
+                    flat
+                    hide-details
+                  ></v-select>
+                </div>
+                <v-divider></v-divider>
+                <div class="px-3" style="height: 48px;">
+                  <v-text-field
+                    label="Solo"
+                    solo
+                    flat
+                    hide-details
+                  ></v-text-field>
+                </div>
+                <v-divider></v-divider>
+                <div class="pa-3" style="height: 48px;">
+                  <span class="px-3">1200</span>
+                </div>
+                <v-divider></v-divider>
+                <div class="px-3" style="height: 48px;">
+                  <v-text-field
+                    label="Solo"
+                    solo
+                    flat
+                    hide-details
+                  ></v-text-field>
+                </div>
+                <v-divider></v-divider>
+                <div class="pa-3" style="height: 48px;">
+                  <span class="px-3">240</span>
+                </div>
+                <v-divider></v-divider>
+                <div class="px-3" style="height: 48px;">
+                  <v-text-field
+                    label="Solo"
+                    solo
+                    flat
+                    hide-details
+                  ></v-text-field>
+                </div>
+                <v-divider></v-divider>
+                <div class="pa-3" style="height: 48px;">
+                  <span class="px-3">144</span>
+                </div>
+                <v-divider></v-divider>
+                <div class="pa-3" style="height: 48px;">
+                  <span class="px-3">1296</span>
+                </div>
+                <v-divider></v-divider>
+                <div class="px-3 text-xs-center" style="height: 48px;">
+                  <v-btn color="info">Info</v-btn>
+                </div>
+                <v-divider></v-divider>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
+      </v-flex>
+    </v-layout>
+    <v-layout row wrap style="min-height: 100px; height: 312px; overflow: auto;">
+      <v-flex
+        xs4
+      >
+        <v-layout row wrap>
+          <v-flex>
             <v-card
               flat
-              width="318"
             >
-              <div class="px-3" style="height: 48px;">
-                <v-select
-                  label="Standard"
-                  solo
-                  flat
-                  hide-details
-                ></v-select>
-              </div>
+              <div class="pa-3 text-xs-center" style="height: 48px;">Остаток</div>
               <v-divider></v-divider>
-              <div class="px-3" style="height: 48px;">
-                <v-select
-                  label="Standard"
-                  solo
-                  flat
-                  hide-details
-                ></v-select>
-              </div>
+              <template v-for="n in 10">
+                <div class="pa-3 text-xs-center" style="height: 48px;" :key="n">351</div>
+                <v-divider :key="'divider-' + n"></v-divider>
+              </template>
+            </v-card>
+          </v-flex>
+          <v-flex>
+            <v-card
+              flat
+            >
+              <div class="pa-3 text-xs-center" style="height: 48px;">Тип</div>
               <v-divider></v-divider>
-              <div class="px-3" style="height: 48px;">
-                <v-select
-                  label="Standard"
-                  solo
-                  flat
-                  hide-details
-                ></v-select>
-              </div>
+              <template v-for="n in 10">
+                <div class="pa-3 text-xs-center" style="height: 48px;" :key="n">Эквадор</div>
+                <v-divider :key="'divider-' + n"></v-divider>
+              </template>
+            </v-card>
+          </v-flex>
+          <v-flex>
+            <v-card
+              flat
+            >
+              <div class="pa-3 text-xs-center" style="height: 48px;">Наименование</div>
               <v-divider></v-divider>
-              <div class="px-3" style="height: 48px;">
-                <v-text-field
-                  label="Solo"
-                  solo
-                  flat
-                  hide-details
-                ></v-text-field>
-              </div>
+              <template v-for="n in 10">
+                <div class="pa-3 text-xs-center" style="height: 48px;"
+                  :key="n"
+                >Цветок {{ n }}</div>
+                <v-divider :key="'divider-' + n"></v-divider>
+              </template>
+            </v-card>
+          </v-flex>
+          <v-flex>
+            <v-card
+              flat
+            >
+              <div class="pa-3 text-xs-center" style="height: 48px;">Цена</div>
               <v-divider></v-divider>
-              <div class="pa-3" style="height: 48px;">
-                <span class="px-3">1200</span>
-              </div>
-              <v-divider></v-divider>
-              <div class="px-3" style="height: 48px;">
-                <v-text-field
-                  label="Solo"
-                  solo
-                  flat
-                  hide-details
-                ></v-text-field>
-              </div>
-              <v-divider></v-divider>
-              <div class="pa-3" style="height: 48px;">
-                <span class="px-3">240</span>
-              </div>
-              <v-divider></v-divider>
-              <div class="px-3" style="height: 48px;">
-                <v-text-field
-                  label="Solo"
-                  solo
-                  flat
-                  hide-details
-                ></v-text-field>
-              </div>
-              <v-divider></v-divider>
-              <div class="pa-3" style="height: 48px;">
-                <span class="px-3">144</span>
-              </div>
-              <v-divider></v-divider>
-              <div class="pa-3" style="height: 48px;">
-                <span class="px-3">1296</span>
-              </div>
-              <v-divider></v-divider>
-              <div class="px-3 text-xs-center" style="height: 48px;">
-                <v-btn color="info">Info</v-btn>
-              </div>
-              <v-divider></v-divider>
+              <template v-for="n in 10">
+                <div class="pa-3 text-xs-center" style="height: 48px;" :key="n">123</div>
+                <v-divider :key="'divider-' + n"></v-divider>
+              </template>
             </v-card>
           </v-flex>
         </v-layout>
       </v-flex>
-    </v-layout>
-    <v-layout row wrap>
-      <v-flex
-        xs4
-      >
-      </v-flex>
       <v-flex
         xs8
       >
+        <v-container
+          id="scroll-block-bottom"
+          style="overflow: hidden;"
+          class="scroll-y pa-0 right-block"
+        >
+          <v-layout row>
+            <v-flex
+              v-for="n in 10"
+              :key="n"
+            >
+              <v-card
+                flat
+                width="300"
+                style="border-left: 1px solid #ccc; font-size: 16px;"
+              >
+                <div class="pa-3 text-xs-center" style="height: 48px;">Кол-во</div>
+                <v-divider></v-divider>
+                <template v-for="j in 10">
+                  <div class="px-3" style="height: 48px;" :key="j">
+                    <v-text-field
+                      label="Solo"
+                      solo
+                      flat
+                      hide-details
+                    ></v-text-field>
+                  </div>
+                  <v-divider :key="'divider-' + j"></v-divider>
+                </template>
+              </v-card>
+            </v-flex>
+          </v-layout>
+        </v-container>
       </v-flex>
     </v-layout>
   </v-container>
@@ -126,6 +219,7 @@ export default {
   name: 'Home',
   data() {
     return {
+      offsetLeft: 0,
       propsBouquet: [
         'Флорист',
         'Клиент',
@@ -139,7 +233,45 @@ export default {
         'Всего к оплате',
         '',
       ],
+      propsGoodsTable: [
+        {
+          text: 'Остаток',
+          value: 'store',
+          align: 'center',
+          sortable: false,
+        },
+        {
+          text: 'Тип',
+          value: 'type',
+          align: 'center',
+          sortable: false,
+        },
+        {
+          text: 'Наименование',
+          value: 'name',
+          align: 'center',
+          sortable: false,
+        },
+        {
+          text: 'Цена',
+          value: 'price',
+          align: 'center',
+          sortable: false,
+        },
+      ],
     };
+  },
+  methods: {
+    onScroll: function onScroll(e) {
+      console.log(e);
+      const scroll = e.target.scrollLeft;
+
+      if (e.target.id === 'scroll-block-top') {
+        document.getElementById('scroll-block-bottom').scrollLeft = scroll;
+      } else {
+        // document.getElementById('scroll-block-top').scrollLeft = scroll;
+      }
+    },
   },
 };
 </script>

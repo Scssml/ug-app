7ad1932a37a232c13b2df06d3a4fa401adfc8d8f
pavel.karpovich <pavel.karpovich@outlook.com>
2020-01-14 22:38:09 +0300

    feature: edit good sort index from table

--- src/views/Goods/List.vue
+++ src/views/Goods/List.vue
@@ -1,25 +1,18 @@
 <template>
-  <v-container
-    fluid
-    class="pa-0"
-  >
-    <v-dialog
-      :value="loadingDialog"
-      persistent
-      max-width="320px"
-    >
+  <v-container fluid class="pa-0">
+    <v-dialog :value="loadingDialog" persistent max-width="320px">
       <v-list>
         <v-list-tile
           v-for="(item, index) in loadingData"
           :key="index"
           avatar
-          :color="(item.error) ? 'red' : item.color"
+          :color="item.error ? 'red' : item.color"
         >
           <v-list-tile-avatar>
             <v-progress-circular
               :value="100"
               :size="30"
-              :color="(item.error) ? 'red' : item.color"
+              :color="item.error ? 'red' : item.color"
               :indeterminate="item.loading"
             ></v-progress-circular>
           </v-list-tile-avatar>
@@ -31,39 +24,19 @@
       </v-list>
     </v-dialog>
     <template v-if="!loadingDialog">
-      <v-dialog
-        v-model="dialogForm"
-        persistent
-        max-width="420px"
-      >
-        <template
-          v-if="dialogForm"
-        >
-          <good-add
-            @cancel="closeDialogAdd()"
-          ></good-add>
+      <v-dialog v-model="dialogForm" persistent max-width="420px">
+        <template v-if="dialogForm">
+          <good-add @cancel="closeDialogAdd()"></good-add>
         </template>
       </v-dialog>
       <v-card>
-        <v-alert
-          :value="createdSuccess"
-          type="success"
-          class="my-0"
-        >
+        <v-alert :value="createdSuccess" type="success" class="my-0">
           {{ formAlertTitle }}
         </v-alert>
-        <v-form
-          ref="form"
-          lazy-validation
-        >
+        <v-form ref="form" lazy-validation>
           <v-card-title>
-            <v-layout
-              row
-              wrap
-            >
-              <v-flex
-                xs2
-              >
+            <v-layout row wrap>
+              <v-flex xs2>
                 <v-select
                   label="Тип изменения"
                   :items="typeEdit"
@@ -75,21 +48,19 @@
                   class="pr-4"
                 ></v-select>
               </v-flex>
-              <v-flex
-                xs2
-              >
+              <v-flex xs2>
                 <v-text-field
                   label="Закупка"
                   :value="dataEdit.purchase"
                   hide-details
                   class="pr-4"
-                  :rules="[v => ((v !== '') && (v !== undefined)) || 'Заполните поле']"
+                  :rules="[
+                    v => (v !== '' && v !== undefined) || 'Заполните поле'
+                  ]"
                   @change="dataEdit.purchase = exelCalc($event)"
                 ></v-text-field>
               </v-flex>
-              <v-flex
-                xs2
-              >
+              <v-flex xs2>
                 <v-text-field
                   label="Приход"
                   :value="arrival"
@@ -98,9 +69,7 @@
                   readonly
                 ></v-text-field>
               </v-flex>
-              <v-flex
-                xs1
-              >
+              <v-flex xs1>
                 <v-text-field
                   label="Наценка"
                   :value="markup + '%'"
@@ -109,16 +78,14 @@
                   readonly
                 ></v-text-field>
               </v-flex>
-              <v-flex
-                xs3
-              >
+              <v-flex xs3>
                 <!-- <v-text-field
-                  label="Компания"
-                  v-model="dataEdit.company"
-                  hide-details
-                  class="pr-4"
-                  :rules="[v => !!v || 'Заполните поле']"
-                ></v-text-field> -->
+                                                  label="Компания"
+                                                  v-model="dataEdit.company"
+                                                  hide-details
+                                                  class="pr-4"
+                                                  :rules="[v => !!v || 'Заполните поле']"
+                                                ></v-text-field> -->
                 <v-autocomplete
                   label="Компания"
                   :items="clientsList"
@@ -133,13 +100,8 @@
                   clearable
                 ></v-autocomplete>
               </v-flex>
-              <v-flex
-                xs2
-              >
-                <v-btn
-                  color="info"
-                  @click="submitForm"
-                >Сохранить</v-btn>
+              <v-flex xs2>
+                <v-btn color="info" @click="submitForm">Сохранить</v-btn>
               </v-flex>
             </v-layout>
           </v-card-title>
@@ -155,10 +117,7 @@
             hide-details
           ></v-text-field>
           <v-spacer></v-spacer>
-          <v-btn
-            color="info"
-            to="/goods/history/"
-          >История</v-btn>
+          <v-btn color="info" to="/goods/history/">История</v-btn>
         </v-card-title>
         <v-data-table
           :headers="headersTable"
@@ -170,69 +129,13 @@
           disable-initial-sort
         >
           <template slot="items" slot-scope="props">
-            <td>
-              <v-text-field
-                label="Склад"
-                solo
-                flat
-                hide-details
-                :value="+props.item.stockBalance + +props.item.count"
-                class="scs-small"
-                readonly
-              ></v-text-field>
-            </td>
-            <td>
-              <v-text-field
-                label="Название"
-                solo
-                flat
-                hide-details
-                v-model="props.item.name"
-                class="scs-small"
-              ></v-text-field>
-            </td>
-            <td>
-              <v-text-field
-                label="Сортировка"
-                solo
-                flat
-                hide-details
-                v-model="props.item.sortIndex"
-                class="scs-small"
-              ></v-text-field>
-            </td>
-            <td>
-              <v-text-field
-                label="Цена"
-                solo
-                flat
-                hide-details
-                type="number"
-                v-model="props.item.price"
-                class="scs-small"
-              ></v-text-field>
-            </td>
-            <td>
-              <v-text-field
-                label="Кол-во"
-                solo
-                flat
-                hide-details
-                type="text"
-                :value="props.item.count"
-                @change="props.item.count = exelCalc($event)"
-                class="scs-small"
-              ></v-text-field>
-            </td>
-            <td class="text-xs-right" style="width: 7%;"></td>
+            <list-item
+              :props="props"
+              @exel-calc="exelCalc"
+            />
           </template>
         </v-data-table>
-        <v-btn
-          fab
-          color="info"
-          class="mx-4"
-          @click="dialogForm = true;"
-        >
+        <v-btn fab color="info" class="mx-4" @click="dialogForm = true">
           <v-icon dark>add</v-icon>
         </v-btn>
       </v-card>
@@ -241,77 +144,81 @@
 </template>
 
 <script>
-import GoodAdd from './add.vue';
+import GoodAdd from "./add.vue";
+import ListItem from "./ListItem";
 
 export default {
-  name: 'Goods',
+  name: "Goods",
   components: {
     GoodAdd,
+    ListItem
   },
   data() {
     return {
       loadingData: [
         {
-          title: 'Получение товаров',
+          title: "Получение товаров",
           error: false,
           loading: true,
-          color: 'indigo',
-          id: 'goods',
-        },
+          color: "indigo",
+          id: "goods"
+        }
       ],
       typeEdit: [],
       dataEdit: {
         type: 0,
-        company: '',
-        purchase: 0,
+        company: "",
+        purchase: 0
       },
-      search: '',
+      search: "",
       headersTable: [
         {
-          text: 'Остаток',
-          align: 'left',
-          value: 'stockBalance',
+          text: "Остаток",
+          align: "left",
+          value: "stockBalance"
         },
         {
-          text: 'Название',
-          align: 'left',
-          value: 'name',
+          text: "Название",
+          align: "left",
+          value: "name"
         },
         {
-          text: 'Сортировка',
-          align: 'left',
-          value: 'sortIndex',
+          text: "Сортировка",
+          align: "left",
+          value: "sortIndex"
         },
         {
-          text: 'Цена',
-          align: 'left',
-          value: 'price',
+          text: "Цена",
+          align: "left",
+          value: "price"
         },
         {
-          text: 'Кол-во',
-          align: 'left',
-          value: 'count',
+          text: "Кол-во",
+          align: "left",
+          value: "count"
         },
         {
-          text: '',
-          align: 'right',
+          text: "",
+          align: "right",
           sortable: false,
-          value: 'action',
-        },
+          value: "action"
+        }
       ],
       goodsList: [],
       createdSuccess: false,
       dialogForm: false,
-      clientsList: [],
+      clientsList: []
     };
   },
   computed: {
     loadingDialog() {
-      const loadData = this.loadingData.filter(item => !item.error && !item.loading);
-      return (loadData.length === this.loadingData.length) ? 0 : 1;
+      const loadData = this.loadingData.filter(
+        item => !item.error && !item.loading
+      );
+      return loadData.length === this.loadingData.length ? 0 : 1;
     },
     formAlertTitle() {
-      return 'Остатки изменены';
+      return "Остатки изменены";
     },
     arrival() {
       const arrival = this.goodsList.reduce((sum, item) => {
@@ -324,17 +231,17 @@ export default {
     markup() {
       let markupVal = 0;
       if (this.arrival !== 0) {
-        markupVal = 100 - ((this.dataEdit.purchase * 100) / this.arrival);
+        markupVal = 100 - (this.dataEdit.purchase * 100) / this.arrival;
       }
 
       return +markupVal.toFixed(2);
-    },
+    }
   },
   methods: {
     exelCalc(val) {
       /* eslint no-eval: 0 */
 
-      const value = (val.length > 1) ? val.replace(/^0+/, '') : val;
+      const value = val.length > 1 ? val.replace(/^0+/, "") : val;
       console.log(value);
 
       // return value;
@@ -342,71 +249,85 @@ export default {
     },
     getGoodsList() {
       const itemParams = {
-        type: 'goods',
+        type: "goods",
         sort: {
-          sortIndex: 'asc',
-        },
+          sortIndex: "asc"
+        }
       };
 
-      const successData = 'Товары получены!';
-      const errorData = 'Ошибка получения товаров!';
+      const successData = "Товары получены!";
+      const errorData = "Ошибка получения товаров!";
 
-      this.$store.dispatch('getItemsList', itemParams).then((response) => {
-        this.goodsList = response.map((item) => {
-          const good = item;
-          good.count = 0;
-          good.oldPrice = good.price;
-          return good;
+      this.$store
+        .dispatch("getItemsList", itemParams)
+        .then(response => {
+          this.goodsList = response.map(item => {
+            const good = item;
+            good.count = 0;
+            good.oldPrice = good.price;
+            return good;
+          });
+
+          const loadData = this.loadingData.find(
+            item => item.id === itemParams.type
+          );
+          loadData.title = successData;
+          loadData.loading = false;
+        })
+        .catch(() => {
+          const loadData = this.loadingData.find(
+            item => item.id === itemParams.type
+          );
+          loadData.title = errorData;
+          loadData.error = true;
         });
-
-        const loadData = this.loadingData.find(item => item.id === itemParams.type);
-        loadData.title = successData;
-        loadData.loading = false;
-      }).catch(() => {
-        const loadData = this.loadingData.find(item => item.id === itemParams.type);
-        loadData.title = errorData;
-        loadData.error = true;
-      });
     },
     getPurchaseTypesList() {
       const itemParams = {
-        type: 'purchase-types',
+        type: "purchase-types"
       };
 
-      this.$store.dispatch('getItemsList', itemParams).then((response) => {
-        this.typeEdit = response.map((item) => {
-          item.id = +item.id;
-          return item;
+      this.$store
+        .dispatch("getItemsList", itemParams)
+        .then(response => {
+          this.typeEdit = response.map(item => {
+            item.id = +item.id;
+            return item;
+          });
+        })
+        .catch(() => {
+          console.log("error");
         });
-      }).catch(() => {
-        console.log('error');
-      });
     },
     getClientsList() {
       const itemParams = {
-        type: 'clients',
+        type: "clients",
         filter: {
           active: true,
-          type: 2,
-        },
+          type: 2
+        }
       };
 
-      this.$store.dispatch('getItemsList', itemParams).then((response) => {
-        this.clientsList = response.map((item) => {
-          item.id = +item.id;
-          return item;
+      this.$store
+        .dispatch("getItemsList", itemParams)
+        .then(response => {
+          this.clientsList = response.map(item => {
+            item.id = +item.id;
+            return item;
+          });
+        })
+        .catch(() => {
+          console.log("error");
         });
-      }).catch(() => {
-        console.log('error');
-      });
     },
     clientsFilter(item, queryText) {
       const textOne = item.name.toLowerCase();
-      const textTwo = item.phone.replace(/[^0-9]/gim, '');
+      const textTwo = item.phone.replace(/[^0-9]/gim, "");
       const searchText = queryText.toLowerCase();
 
-      return textOne.indexOf(searchText) > -1 ||
-        textTwo.indexOf(searchText) > -1;
+      return (
+        textOne.indexOf(searchText) > -1 || textTwo.indexOf(searchText) > -1
+      );
     },
     submitForm() {
       const validate = this.$refs.form.validate();
@@ -418,27 +339,30 @@ export default {
         //   return good;
         // });
 
-        const purchaseGoods = this.goodsList.map((item) => {
+        const purchaseGoods = this.goodsList.map(item => {
           const good = {
             estimate: item.count,
             newPrice: item.price,
             oldPrice: item.oldPrice,
             stockQuantity: item.stockBalance,
-            good: item.id,
+            good: item.id
           };
 
           return good;
         });
 
         const propsItem = Object.assign({}, this.dataEdit);
-        [propsItem.purchasedGoods, propsItem.arrival] = [purchaseGoods, this.arrival];
+        [propsItem.purchasedGoods, propsItem.arrival] = [
+          purchaseGoods,
+          this.arrival
+        ];
 
         const itemParams = {
-          type: 'purchase',
-          props: propsItem,
+          type: "purchase",
+          props: propsItem
         };
 
-        this.$store.dispatch('addItem', itemParams).then(() => {
+        this.$store.dispatch("addItem", itemParams).then(() => {
           this.createdSuccess = true;
           this.getGoodsList();
           setTimeout(() => {
@@ -446,7 +370,7 @@ export default {
           }, 1000);
         });
 
-        this.goodsList.forEach((elem) => {
+        this.goodsList.forEach(elem => {
           const propsGood = Object.assign({}, elem);
           propsGood.stockBalance = +propsGood.stockBalance + +propsGood.count;
           delete propsGood.id;
@@ -455,12 +379,12 @@ export default {
           propsGood.price = +propsGood.price;
 
           const goodParams = {
-            type: 'goods',
+            type: "goods",
             props: propsGood,
-            id: elem.id,
+            id: elem.id
           };
 
-          this.$store.dispatch('updateItem', goodParams);
+          this.$store.dispatch("updateItem", goodParams);
         });
       }
     },
@@ -470,46 +394,45 @@ export default {
     closeDialogAdd() {
       this.getGoodsList();
       this.dialogForm = false;
-    },
+    }
   },
   mounted() {
     this.getGoodsList();
     this.getPurchaseTypesList();
     this.getClientsList();
-  },
+  }
 };
 </script>
 
 <style lang="scss">
-  .v-table {
+.v-table {
+  tr:nth-child(even) {
+    td {
+      background: #f9f9f9;
+    }
 
-    tr:nth-child(even) {
-
+    &:hover {
       td {
-        background: #f9f9f9;
-      }
-
-      &:hover {
-
-        td {
-          background: #eee;
-        }
+        background: #eee;
       }
     }
   }
+}
 
-  .scs-small {
-    &.v-text-field {
-      .v-input__control {
-        min-height: 30px;
-      }
-      input {
-        max-height: 30px;
-        padding: 5px 0;
-      }
-      .v-select__selection--comma {
-        margin: 6px 4px 6px 0;
-      }
+.scs-small {
+  &.v-text-field {
+    .v-input__control {
+      min-height: 30px;
+    }
+
+    input {
+      max-height: 30px;
+      padding: 5px 0;
+    }
+
+    .v-select__selection--comma {
+      margin: 6px 4px 6px 0;
     }
   }
+}
 </style>
---
+++ src/views/Goods/ListItem.vue
@@ -0,0 +1,82 @@
+<template>
+  <tr>
+    <td>
+      <v-text-field
+        label="Склад"
+        solo
+        flat
+        hide-details
+        :value="+props.item.stockBalance + +props.item.count"
+        class="scs-small"
+        readonly
+      ></v-text-field>
+    </td>
+    <td>
+      <v-text-field
+        label="Название"
+        solo
+        flat
+        hide-details
+        v-model="props.item.name"
+        class="scs-small"
+      ></v-text-field>
+    </td>
+    <td>
+      <v-text-field
+        label="Сортировка"
+        solo
+        flat
+        hide-details
+        v-model="props.item.sortIndex"
+        class="scs-small"
+        @blur="changeSortIndex"
+      ></v-text-field>
+    </td>
+    <td>
+      <v-text-field
+        label="Цена"
+        solo
+        flat
+        hide-details
+        type="number"
+        v-model="props.item.price"
+        class="scs-small"
+      ></v-text-field>
+    </td>
+    <td>
+      <v-text-field
+        label="Кол-во"
+        solo
+        flat
+        hide-details
+        type="text"
+        :value="props.item.count"
+        @change="props.item.count = exelCalc($event)"
+        class="scs-small"
+      ></v-text-field>
+    </td>
+    <td class="text-xs-right" style="width: 7%;"></td>
+  </tr>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  name: "ListItem",
+  props: ["props", "exelCalc"],
+  methods: {
+    changeSortIndex(e) {
+      const { id } = this.props.item;
+
+      axios
+        .put(`/goods/${id}`, {
+          sortIndex: +e.target.value
+        })
+        .then(() => {});
+    }
+  }
+};
+</script>
+
+<style scoped></style>

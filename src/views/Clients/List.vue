387c85fdce23ea52a50ec369d990a6186d94dba4
pavel.karpovich <pavel.karpovich@outlook.com>
2020-02-14 09:10:44 +0300

    wip: clients list

--- src/views/Clients/List.vue
+++ src/views/Clients/List.vue
@@ -1,25 +1,18 @@
 <template>
-  <v-container
-    fluid
-    class="pa-0"
-  >
-    <v-dialog
-      :value="loadingDialog"
-      persistent
-      max-width="320px"
-    >
+  <v-container fluid class="pa-0">
+    <v-dialog :value="loadingDialog" persistent max-width="320px">
       <v-list>
         <v-list-tile
           v-for="(item, index) in loadingData"
           :key="index"
           avatar
-          :color="(item.error) ? 'red' : item.color"
+          :color="item.error ? 'red' : item.color"
         >
           <v-list-tile-avatar>
             <v-progress-circular
               :value="100"
               :size="30"
-              :color="(item.error) ? 'red' : item.color"
+              :color="item.error ? 'red' : item.color"
               :indeterminate="item.loading"
             ></v-progress-circular>
           </v-list-tile-avatar>
@@ -33,10 +26,7 @@
     <template v-if="!loadingDialog">
       <v-card>
         <v-card-title>
-          <v-layout
-            row
-            wrap
-          >
+          <v-layout row wrap>
             <!-- <v-flex
               xs4
               class="px-3"
@@ -49,13 +39,10 @@
                 hide-details
               ></v-text-field>
             </v-flex> -->
-            <v-flex
-              xs2
-              class="px-3"
-            >
+            <v-flex xs2 class="px-3">
               <v-select
                 label="Тип клиента"
-                :items="[{id: '', name: 'Все'}].concat(typeClient)"
+                :items="[{ id: '', name: 'Все' }].concat(typeClient)"
                 item-text="name"
                 item-value="id"
                 v-model="filter.typeId"
@@ -66,15 +53,11 @@
           </v-layout>
           <v-spacer></v-spacer>
 
-          <v-dialog
-            v-model="dialogForm"
-            persistent
-            max-width="420px"
-          >
-            <v-btn slot="activator" color="primary" dark class="mb-2">Добавить</v-btn>
-            <template
-              v-if="dialogForm"
+          <v-dialog v-model="dialogForm" persistent max-width="420px">
+            <v-btn slot="activator" color="primary" dark class="mb-2"
+              >Добавить</v-btn
             >
+            <template v-if="dialogForm">
               <client-edit
                 v-if="editedId"
                 :id="editedId"
@@ -85,10 +68,7 @@
                 :id="deleteId"
                 @cancel="closeDialog()"
               ></client-delete>
-              <client-add
-                v-else
-                @cancel="closeDialog()"
-              ></client-add>
+              <client-add v-else @cancel="closeDialog()"></client-add>
             </template>
           </v-dialog>
         </v-card-title>
@@ -101,27 +81,29 @@
           no-results-text="Клиентов не найдено"
           :search="search"
           :pagination.sync="pagination"
-          :loading="tableLoading"
+          :loading="!!$apollo.queries.clientsList.loading"
         >
           <template slot="items" slot-scope="props">
-            <td class="text-xs-right" style="width: 30px;">{{ props.item.id }}</td>
+            <td class="text-xs-right" style="width: 30px;">
+              {{ props.item.id }}
+            </td>
             <td>{{ props.item.name }}</td>
             <td>{{ props.item.phone }}</td>
             <td>
-              {{ props.item.typeName }}
+              {{ props.item.clientType.name }}
             </td>
             <td>
               <template v-for="(item, index) in props.item.responsible">
                 <p :key="index">
                   {{ item.name }}
-                  <br>{{ item.phone }}
+                  <br />{{ item.phone }}
                 </p>
               </template>
             </td>
             <td class="text-xs-right">{{ props.item.bill }}</td>
             <td class="text-xs-right">{{ props.item.discountPercent }}</td>
             <td class="text-xs-right">
-              {{ (!!props.item.isActive) ? 'Да' : 'Нет' }}
+              {{ !!props.item.isActive ? "Да" : "Нет" }}
             </td>
             <td class="text-xs-right" style="width: 170px;">
               <v-icon
@@ -138,16 +120,16 @@
               >
                 local_florist
               </v-icon>
-              <v-icon
-                @click="editItem(props.item.id)"
-                title="Изменить"
-              >
+              <v-icon @click="editItem(props.item.id)" title="Изменить">
                 edit
               </v-icon>
               <v-icon
                 @click="deleteItem(props.item.id)"
                 class="ml-2"
-                v-if="props.item.id > 0 && $store.getters.getAuthUserGroup.code === 'admin'"
+                v-if="
+                  props.item.id > 0 &&
+                    $store.getters.getAuthUserGroup.code === 'admin'
+                "
                 title="Удалить"
               >
                 delete
@@ -155,16 +137,8 @@
             </td>
           </template>
         </v-data-table>
-        <v-layout
-          row
-          wrap
-          justify-space-around
-          class="py-2"
-        >
-          <v-flex
-            xs2
-            class="px-3"
-          >
+        <v-layout row wrap justify-space-around class="py-2">
+          <v-flex xs2 class="px-3">
             <v-text-field
               label="Количество на странице"
               v-model.number="take"
@@ -172,10 +146,7 @@
               @change="changeShowElem()"
             ></v-text-field>
           </v-flex>
-          <v-flex
-            xs10
-            class="text-xs-right px-3"
-          >
+          <v-flex xs10 class="text-xs-right px-3">
             <v-btn
               small
               color="info"
@@ -202,106 +173,160 @@
 </template>
 
 <script>
-import ClientEdit from './edit.vue';
-import ClientAdd from './add.vue';
-import ClientDelete from './delete.vue';
+import ClientEdit from "./edit.vue";
+import ClientAdd from "./add.vue";
+import ClientDelete from "./delete.vue";
+import gql from "graphql-tag";
 
 export default {
-  name: 'Clients',
+  name: "Clients",
   components: {
     ClientEdit,
     ClientAdd,
-    ClientDelete,
+    ClientDelete
   },
   data() {
     return {
       loadingData: [
         {
-          title: 'Получение клиентов',
+          title: "Получение клиентов",
           error: false,
-          loading: true,
-          color: 'indigo',
-          id: 'clients',
-        },
+          loading: false,
+          color: "indigo",
+          id: "clients"
+        }
       ],
       filter: {
-        typeId: '',
+        typeId: ""
       },
       typeClient: [],
-      search: '',
+      search: "",
       headersTable: [
         {
-          text: 'ID',
-          align: 'right',
-          value: 'id',
-          sortable: false,
+          text: "ID",
+          align: "right",
+          value: "id",
+          sortable: false
         },
         {
-          text: 'Клиент',
-          align: 'left',
-          value: 'name',
-          sortable: false,
+          text: "Клиент",
+          align: "left",
+          value: "name",
+          sortable: false
         },
         {
-          text: 'Телефон',
-          align: 'left',
-          value: 'phone',
-          sortable: false,
+          text: "Телефон",
+          align: "left",
+          value: "phone",
+          sortable: false
         },
         {
-          text: 'Тип',
-          align: 'left',
-          value: 'type',
-          sortable: false,
+          text: "Тип",
+          align: "left",
+          value: "type",
+          sortable: false
         },
         {
-          text: 'Ответственный',
-          align: 'left',
-          value: 'responsible',
-          sortable: false,
+          text: "Ответственный",
+          align: "left",
+          value: "responsible",
+          sortable: false
         },
         {
-          text: 'Счет',
-          align: 'right',
-          value: 'bill',
-          sortable: false,
+          text: "Счет",
+          align: "right",
+          value: "bill",
+          sortable: false
         },
         {
-          text: 'Скидка',
-          align: 'right',
-          value: 'sale',
-          sortable: false,
+          text: "Скидка",
+          align: "right",
+          value: "sale",
+          sortable: false
         },
         {
-          text: 'Активность',
-          align: 'right',
-          value: 'isActive',
-          sortable: false,
+          text: "Активность",
+          align: "right",
+          value: "isActive",
+          sortable: false
         },
         {
-          text: '',
-          align: 'right',
+          text: "",
+          align: "right",
           sortable: false,
-          value: 'action',
-        },
+          value: "action"
+        }
       ],
       dialogForm: false,
       editedId: 0,
       clientsList: [],
       deleteId: 0,
       pagination: {
-        rowsPerPage: -1,
+        rowsPerPage: -1
       },
       take: 20,
       page: 0,
-      tableLoading: false,
+      tableLoading: false
     };
+  },
+  apollo: {
+    clientsList: {
+      query: gql`
+        query ClientsList(
+          $clientTypeId: bigint_comparison_exp
+          $limit: Int
+          $offset: Int
+        ) {
+          clientsList: clients(
+            order_by: { id: desc }
+            limit: $limit
+            offset: $offset
+            where: {clientType: {id: {_eq: $clientTypeId}}}
+          ) {
+            id
+            name
+            phone
+            address
+            birthDay
+            floor
+            flat
+            entrance
+            clientType {
+              id
+              name
+            }
+            responsible {
+              id
+              name
+              phone
+            }
+            bill
+            discountPercent: sale
+            active
+          }
+        }
+      `,
+      variables() {
+        return {
+          // managerId: this.selectedManagerId
+          //   ? {
+          //       _eq: this.selectedManagerId
+          //     }
+          //   : undefined,
+          clientTypeId: undefined,
+          offset: this.page * this.take,
+          limit: this.take
+        };
+      }
+    }
   },
   computed: {
     loadingDialog: function loadingDialog() {
-      const loadData = this.loadingData.filter(item => !item.error && !item.loading);
-      return (loadData.length === this.loadingData.length) ? 0 : 1;
-    },
+      const loadData = this.loadingData.filter(
+        item => !item.error && !item.loading
+      );
+      return loadData.length === this.loadingData.length ? 0 : 1;
+    }
   },
   methods: {
     customFilter: function customFilter(items) {
@@ -323,71 +348,85 @@ export default {
       this.getClientsList();
     },
     getClientsList: function getClientsList(loading = true) {
-      if (loading) {
-        this.tableLoading = true;
-        this.reviewsList = [];
-      }
-
-      const orderFilter = {};
-
-      Object.keys(this.filter).forEach((key) => {
-        const val = this.filter[key];
-
-        if (val) {
-          orderFilter[key] = val;
-        }
-      });
-
-      const itemParams = {
-        type: 'clients',
-        sort: {
-          id: 'asc',
-        },
-        filter: orderFilter,
-        skip: this.page * this.take,
-        take: this.take,
-      };
-
-      const successData = 'Клиенты получены!';
-      const errorData = 'Ошибка получения клиентов!';
-
-      this.$store.dispatch('getItemsList', itemParams).then((response) => {
-        this.clientsList = response.map((item, index, clients) => {
-          const elem = Object.assign({}, item);
-          const itemTypeClient = this.typeClient.find(client => client.id === elem.type);
-          elem.typeName = (itemTypeClient) ? itemTypeClient.name : '-';
-
-          elem.responsible = [];
-          if (elem.id !== 0) {
-            elem.responsible = clients.filter(client => +client.referenceId === elem.id);
-          }
-          return elem;
-        });
-        this.tableLoading = false;
-
-        const loadData = this.loadingData.find(item => item.id === itemParams.type);
-        loadData.title = successData;
-        loadData.loading = false;
-      }).catch(() => {
-        const loadData = this.loadingData.find(item => item.id === itemParams.type);
-        loadData.title = errorData;
-        loadData.error = true;
-      });
+      // if (loading) {
+      //   this.tableLoading = true;
+      //   this.reviewsList = [];
+      // }
+      //
+      // const orderFilter = {};
+      //
+      // Object.keys(this.filter).forEach(key => {
+      //   const val = this.filter[key];
+      //
+      //   if (val) {
+      //     orderFilter[key] = val;
+      //   }
+      // });
+      //
+      // const itemParams = {
+      //   type: "clients",
+      //   sort: {
+      //     id: "asc"
+      //   },
+      //   filter: orderFilter,
+      //   skip: this.page * this.take,
+      //   take: this.take
+      // };
+      //
+      // const successData = "Клиенты получены!";
+      // const errorData = "Ошибка получения клиентов!";
+      //
+      // this.$store
+      //   .dispatch("getItemsList", itemParams)
+      //   .then(response => {
+      //     this.clientsList = response.map((item, index, clients) => {
+      //       const elem = Object.assign({}, item);
+      //       const itemTypeClient = this.typeClient.find(
+      //         client => client.id === elem.type
+      //       );
+      //       elem.typeName = itemTypeClient ? itemTypeClient.name : "-";
+      //
+      //       elem.responsible = [];
+      //       if (elem.id !== 0) {
+      //         elem.responsible = clients.filter(
+      //           client => +client.referenceId === elem.id
+      //         );
+      //       }
+      //       return elem;
+      //     });
+      //     this.tableLoading = false;
+      //
+      //     const loadData = this.loadingData.find(
+      //       item => item.id === itemParams.type
+      //     );
+      //     loadData.title = successData;
+      //     loadData.loading = false;
+      //   })
+      //   .catch(() => {
+      //     const loadData = this.loadingData.find(
+      //       item => item.id === itemParams.type
+      //     );
+      //     loadData.title = errorData;
+      //     loadData.error = true;
+      //   });
     },
     getClientTypeList() {
       const itemParams = {
-        type: 'client-type',
+        type: "client-type"
       };
 
-      this.$store.dispatch('getItemsList', itemParams).then((response) => {
-        this.typeClient = response.map((item) => {
-          item.id = +item.id;
-          return item;
+      this.$store
+        .dispatch("getItemsList", itemParams)
+        .then(response => {
+          this.typeClient = response.map(item => {
+            item.id = +item.id;
+            return item;
+          });
+          this.getClientsList();
+        })
+        .catch(() => {
+          console.log("error");
         });
-        this.getClientsList();
-      }).catch(() => {
-        console.log('error');
-      });
     },
     closeDialog() {
       this.getClientsList();
@@ -410,8 +449,8 @@ export default {
       this.$router.push({ path: `/bouquets/?client=${id}` });
     },
     changeShowElem() {
-      localStorage.setItem('countElemPage', this.take);
-      this.$store.commit('setCountElemPage', this.take);
+      localStorage.setItem("countElemPage", this.take);
+      this.$store.commit("setCountElemPage", this.take);
       this.page = 0;
       this.getClientTypeList();
     },
@@ -422,29 +461,26 @@ export default {
     nextPage() {
       this.page += 1;
       this.getClientTypeList();
-    },
+    }
   },
   mounted() {
     this.getClientTypeList();
-  },
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-  .v-table {
+.v-table {
+  tr:nth-child(even) {
+    td {
+      background: #f9f9f9;
+    }
 
-    tr:nth-child(even) {
-
+    &:hover {
       td {
-        background: #f9f9f9;
-      }
-
-      &:hover {
-
-        td {
-          background: #eee;
-        }
+        background: #eee;
       }
     }
   }
+}
 </style>

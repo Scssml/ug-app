139988675de06a4b208b5bab70b41298211029a2
scssml <scssml@yandex.ru>
2020-02-20 09:20:21 +0400

    fix

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   devServer: {
-    //proxy: 'http://192.168.4.102',
-    proxy: 'http://192.168.4.54',
+    proxy: 'http://192.168.4.102',
+    //proxy: 'http://192.168.4.54',
     // proxy: {
     //   '/api': {
     //     target: 'http://10.0.0.18:3000',
